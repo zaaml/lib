246f81ef01212a3f8dae68df13794656673e916f
xmetropol <xmetropol@zaaml.com>
2020-12-04 01:43:27 +0300

    FloatingDialog CanExecuteCloseDialogCommand IconContentControl theme style added ButtonBase UpdateCanExecute

--- source/UI/Windows/FloatingDialog.cs
+++ source/UI/Windows/FloatingDialog.cs
@@ -12,232 +12,221 @@ using Zaaml.PresentationCore.Theming;
 
 namespace Zaaml.UI.Windows
 {
-  // ReSharper disable once PartialTypeWithSinglePart
-  public partial class FloatingDialog : DialogWindowBase
-  {
-    #region Static Fields and Constants
+	// ReSharper disable once PartialTypeWithSinglePart
+	public partial class FloatingDialog : DialogWindowBase
+	{
+		internal static readonly RoutedCommand CloseDialogCommand = new RoutedCommand();
 
-    internal static readonly RoutedCommand CloseDialogCommand = new RoutedCommand();
+		public static readonly DependencyProperty DefaultFloatingDialogResultProperty = DPM.Register<FloatingDialogResult, FloatingDialog>
+			("DefaultFloatingDialogResult", FloatingDialogResult.None);
 
-    public static readonly DependencyProperty DefaultFloatingDialogResultProperty = DPM.Register<FloatingDialogResult, FloatingDialog>
-      ("DefaultFloatingDialogResult", FloatingDialogResult.None);
+		public static readonly DependencyProperty ButtonsProperty = DPM.Register<FloatingDialogButtons, FloatingDialog>
+			("Buttons", FloatingDialogButtons.Ok, d => d.OnButtonsChanged);
 
-    public static readonly DependencyProperty ButtonsProperty = DPM.Register<FloatingDialogButtons, FloatingDialog>
-      ("Buttons", FloatingDialogButtons.Ok, d => d.OnButtonsChanged);
+		private List<FloatingDialogButton> _actualButtons;
+		private bool _isClosed;
 
-    #endregion
+		static FloatingDialog()
+		{
+			DefaultStyleKeyHelper.OverrideStyleKey<FloatingDialog>();
+		}
 
-    #region Fields
+		public FloatingDialog()
+		{
+			this.OverrideStyleKey<FloatingDialog>();
 
-    private List<FloatingDialogButton> _actualButtons;
-    private bool _isClosed;
+			CommandBindings.Add(new CommandBinding(CloseDialogCommand, ExecutedCloseDialogCommand, CanExecuteCloseDialogCommand));
 
-    #endregion
+			UpdateButtons();
+		}
 
-    #region Ctors
+		protected override IEnumerable<WindowButton> ActualButtons => _actualButtons ?? Enumerable.Empty<WindowButton>();
 
-    static FloatingDialog()
-    {
-      DefaultStyleKeyHelper.OverrideStyleKey<FloatingDialog>();
-    }
+		public FloatingDialogButton ApplyButton { get; } = new FloatingDialogButton {DialogResult = FloatingDialogResult.Apply};
 
-    public FloatingDialog()
-    {
-      this.OverrideStyleKey<FloatingDialog>();
+		public FloatingDialogButtons Buttons
+		{
+			get => (FloatingDialogButtons) GetValue(ButtonsProperty);
+			set => SetValue(ButtonsProperty, value);
+		}
 
-      CommandBindings.Add(new CommandBinding(CloseDialogCommand, ExecutedCloseDialogCommand));
+		public FloatingDialogButton CancelButton { get; } = new FloatingDialogButton {DialogResult = FloatingDialogResult.Cancel};
 
-      UpdateButtons();
-    }
+		public FloatingDialogResult DefaultFloatingDialogResult
+		{
+			get => (FloatingDialogResult) GetValue(DefaultFloatingDialogResultProperty);
+			set => SetValue(DefaultFloatingDialogResultProperty, value);
+		}
 
-    #endregion
+		public FloatingDialogResult FloatingDialogResult { get; set; }
 
-    #region Properties
+		public FloatingDialogButton NoButton { get; } = new FloatingDialogButton {DialogResult = FloatingDialogResult.No};
 
-    protected override IEnumerable<WindowButton> ActualButtons => _actualButtons ?? Enumerable.Empty<WindowButton>();
+		public FloatingDialogButton OkButton { get; } = new FloatingDialogButton {DialogResult = FloatingDialogResult.OK};
 
-    public FloatingDialogButton ApplyButton { get; } = new FloatingDialogButton {DialogResult = FloatingDialogResult.Apply};
+		public FloatingDialogButton YesButton { get; } = new FloatingDialogButton {DialogResult = FloatingDialogResult.Yes};
 
-    public FloatingDialogButtons Buttons
-    {
-      get => (FloatingDialogButtons) GetValue(ButtonsProperty);
-      set => SetValue(ButtonsProperty, value);
-    }
+		protected virtual bool CanExecuteCloseDialogCommand(FloatingDialogResult dialogResult)
+		{
+			return true;
+		}
 
-    public FloatingDialogButton CancelButton { get; } = new FloatingDialogButton {DialogResult = FloatingDialogResult.Cancel};
+		private void CanExecuteCloseDialogCommand(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = e.Parameter is FloatingDialogResult floatingDialogResult && CanExecuteCloseDialogCommand(floatingDialogResult);
+		}
 
-    public FloatingDialogResult DefaultFloatingDialogResult
-    {
-      get => (FloatingDialogResult) GetValue(DefaultFloatingDialogResultProperty);
-      set => SetValue(DefaultFloatingDialogResultProperty, value);
-    }
+		protected override void CloseByEnter()
+		{
+			CloseWithResult(true);
+		}
 
-    public FloatingDialogResult FloatingDialogResult { get; set; }
+		protected override void CloseByEscape()
+		{
+			CloseWithResult(false);
+		}
 
-    public FloatingDialogButton NoButton { get; } = new FloatingDialogButton {DialogResult = FloatingDialogResult.No};
+		public void CloseWithResult(FloatingDialogResult result)
+		{
+			var currentResult = FloatingDialogResult;
 
-    public FloatingDialogButton OkButton { get; } = new FloatingDialogButton {DialogResult = FloatingDialogResult.OK};
+			FloatingDialogResult = result;
 
-    public FloatingDialogButton YesButton { get; } = new FloatingDialogButton {DialogResult = FloatingDialogResult.Yes};
+			_isClosed = false;
+			DialogResult = GetDialogResult(result);
 
-    #endregion
+			if (_isClosed == false)
+				FloatingDialogResult = currentResult;
+		}
 
-    #region  Methods
+		public void CloseWithResult(bool? result)
+		{
+			if (DefaultFloatingDialogResult == FloatingDialogResult.None)
+			{
+				switch (result)
+				{
+					case null:
+						CloseWithResult(DefaultFloatingDialogResult);
+						break;
+					case true:
+						CloseWithResult(GetDefaultOKResult(Buttons));
+						break;
+					default:
+						CloseWithResult(GetDefaultCancelResult(Buttons));
+						break;
+				}
+			}
+			else
+				CloseWithResult(DefaultFloatingDialogResult);
+		}
 
-    protected override void CloseByEnter()
-    {
-      CloseWithResult(true);
-    }
+		private void ExecutedCloseDialogCommand(object sender, ExecutedRoutedEventArgs e)
+		{
+			CloseWithResult((FloatingDialogResult) e.Parameter);
+		}
 
-    protected override void CloseByEscape()
-    {
-      CloseWithResult(false);
-    }
+		private static FloatingDialogResult GetDefaultCancelResult(FloatingDialogButtons buttons)
+		{
+			switch (buttons)
+			{
+				case FloatingDialogButtons.Ok:
+					return FloatingDialogResult.OK;
+				case FloatingDialogButtons.OkCancel:
+				case FloatingDialogButtons.YesNoCancel:
+				case FloatingDialogButtons.OkCancelApply:
+					return FloatingDialogResult.Cancel;
+				case FloatingDialogButtons.YesNo:
+					return FloatingDialogResult.No;
+				default:
+					throw new ArgumentOutOfRangeException(nameof(buttons));
+			}
+		}
 
-    public void CloseWithResult(FloatingDialogResult result)
-    {
-      var currentResult = FloatingDialogResult;
+		private static FloatingDialogResult GetDefaultOKResult(FloatingDialogButtons buttons)
+		{
+			switch (buttons)
+			{
+				case FloatingDialogButtons.Ok:
+					return FloatingDialogResult.OK;
+				case FloatingDialogButtons.OkCancel:
+				case FloatingDialogButtons.OkCancelApply:
+					return FloatingDialogResult.OK;
+				case FloatingDialogButtons.YesNo:
+				case FloatingDialogButtons.YesNoCancel:
+					return FloatingDialogResult.Yes;
+				default:
+					throw new ArgumentOutOfRangeException(nameof(buttons));
+			}
+		}
 
-      FloatingDialogResult = result;
+		private static bool? GetDialogResult(FloatingDialogResult result)
+		{
+			switch (result)
+			{
+				case FloatingDialogResult.None:
+					return null;
+				case FloatingDialogResult.OK:
+					return true;
+				case FloatingDialogResult.Cancel:
+					return false;
+				case FloatingDialogResult.Abort:
+					return false;
+				case FloatingDialogResult.Retry:
+					return true;
+				case FloatingDialogResult.Ignore:
+					return false;
+				case FloatingDialogResult.Yes:
+					return true;
+				case FloatingDialogResult.No:
+					return false;
+				case FloatingDialogResult.Apply:
+					return true;
+				default:
+					throw new ArgumentOutOfRangeException(nameof(result));
+			}
+		}
 
-      _isClosed = false;
-      DialogResult = GetDialogResult(result);
+		private void OnButtonsChanged()
+		{
+			UpdateButtons();
+		}
 
-      if (_isClosed == false)
-        FloatingDialogResult = currentResult;
-    }
+		protected override void OnExecutedCloseCommand(object commandParameter)
+		{
+			if (commandParameter is FloatingDialogResult)
+				FloatingDialogResult = (FloatingDialogResult) commandParameter;
+			else
+				FloatingDialogResult = FloatingDialogResult.None;
 
-    public void CloseWithResult(bool? result)
-    {
-      if (DefaultFloatingDialogResult == FloatingDialogResult.None)
-      {
-        switch (result)
-        {
-          case null:
-            CloseWithResult(DefaultFloatingDialogResult);
-            break;
-          case true:
-            CloseWithResult(GetDefaultOKResult(Buttons));
-            break;
-          default:
-            CloseWithResult(GetDefaultCancelResult(Buttons));
-            break;
-        }
-      }
-      else
-        CloseWithResult(DefaultFloatingDialogResult);
-    }
+			Close();
+		}
 
+		private void UpdateButtons()
+		{
+			DetachButtons();
 
-    private void ExecutedCloseDialogCommand(object sender, ExecutedRoutedEventArgs e)
-    {
-      CloseWithResult((FloatingDialogResult) e.Parameter);
-    }
+			switch (Buttons)
+			{
+				case FloatingDialogButtons.Ok:
+					_actualButtons = new List<FloatingDialogButton> {OkButton};
+					break;
+				case FloatingDialogButtons.OkCancel:
+					_actualButtons = new List<FloatingDialogButton> {OkButton, CancelButton};
+					break;
+				case FloatingDialogButtons.YesNoCancel:
+					_actualButtons = new List<FloatingDialogButton> {YesButton, NoButton, CancelButton};
+					break;
+				case FloatingDialogButtons.YesNo:
+					_actualButtons = new List<FloatingDialogButton> {YesButton, NoButton};
+					break;
+				case FloatingDialogButtons.OkCancelApply:
+					_actualButtons = new List<FloatingDialogButton> {OkButton, CancelButton, ApplyButton};
+					break;
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
 
-    private static FloatingDialogResult GetDefaultCancelResult(FloatingDialogButtons buttons)
-    {
-      switch (buttons)
-      {
-        case FloatingDialogButtons.Ok:
-          return FloatingDialogResult.OK;
-        case FloatingDialogButtons.OkCancel:
-        case FloatingDialogButtons.YesNoCancel:
-        case FloatingDialogButtons.OkCancelApply:
-          return FloatingDialogResult.Cancel;
-        case FloatingDialogButtons.YesNo:
-          return FloatingDialogResult.No;
-        default:
-          throw new ArgumentOutOfRangeException(nameof(buttons));
-      }
-    }
-
-    private static FloatingDialogResult GetDefaultOKResult(FloatingDialogButtons buttons)
-    {
-      switch (buttons)
-      {
-        case FloatingDialogButtons.Ok:
-          return FloatingDialogResult.OK;
-        case FloatingDialogButtons.OkCancel:
-        case FloatingDialogButtons.OkCancelApply:
-          return FloatingDialogResult.OK;
-        case FloatingDialogButtons.YesNo:
-        case FloatingDialogButtons.YesNoCancel:
-          return FloatingDialogResult.Yes;
-        default:
-          throw new ArgumentOutOfRangeException(nameof(buttons));
-      }
-    }
-
-    private static bool? GetDialogResult(FloatingDialogResult result)
-    {
-      switch (result)
-      {
-        case FloatingDialogResult.None:
-          return null;
-        case FloatingDialogResult.OK:
-          return true;
-        case FloatingDialogResult.Cancel:
-          return false;
-        case FloatingDialogResult.Abort:
-          return false;
-        case FloatingDialogResult.Retry:
-          return true;
-        case FloatingDialogResult.Ignore:
-          return false;
-        case FloatingDialogResult.Yes:
-          return true;
-        case FloatingDialogResult.No:
-          return false;
-        case FloatingDialogResult.Apply:
-          return true;
-        default:
-          throw new ArgumentOutOfRangeException(nameof(result));
-      }
-    }
-
-    private void OnButtonsChanged()
-    {
-      UpdateButtons();
-    }
-
-    protected override void OnExecutedCloseCommand(object commandParameter)
-    {
-      if (commandParameter is FloatingDialogResult)
-        FloatingDialogResult = (FloatingDialogResult) commandParameter;
-      else
-        FloatingDialogResult = FloatingDialogResult.None;
-
-      Close();
-    }
-
-    private void UpdateButtons()
-    {
-      DetachButtons();
-
-      switch (Buttons)
-      {
-        case FloatingDialogButtons.Ok:
-          _actualButtons = new List<FloatingDialogButton> {OkButton};
-          break;
-        case FloatingDialogButtons.OkCancel:
-          _actualButtons = new List<FloatingDialogButton> {OkButton, CancelButton};
-          break;
-        case FloatingDialogButtons.YesNoCancel:
-          _actualButtons = new List<FloatingDialogButton> {YesButton, NoButton, CancelButton};
-          break;
-        case FloatingDialogButtons.YesNo:
-          _actualButtons = new List<FloatingDialogButton> {YesButton, NoButton};
-          break;
-        case FloatingDialogButtons.OkCancelApply:
-          _actualButtons = new List<FloatingDialogButton> {OkButton, CancelButton, ApplyButton};
-          break;
-        default:
-          throw new ArgumentOutOfRangeException();
-      }
-
-      AttachButtons();
-    }
-
-    #endregion
-  }
+			AttachButtons();
+		}
+	}
 }
